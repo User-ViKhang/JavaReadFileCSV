c7c07cf8c8871faa0947ca9ba7486dec8eb16d57
User-ViKhang <vikhang11112002@gmail.com>
2023-10-03 16:25:04 +0700

    Lum

--- ReadFileCSV/src/main/java/com/Main.java
+++ ReadFileCSV/src/main/java/com/Main.java
@@ -9,7 +9,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         readFile.readFileCSV("companies.csv");
         System.out.println("Hello");
-        System.out.println("Sài git lần 2");
-        System.out.println("Sua code demo");
+
     }
 }   
