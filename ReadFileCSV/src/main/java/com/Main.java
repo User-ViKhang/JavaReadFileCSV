345e4dc44c576915a61b0808fc102c4c68ecba1d
User-ViKhang <vikhang11112002@gmail.com>
2023-10-03 19:42:09 +0700

    push init code

--- ReadFileCSV/src/main/java/com/Main.java
+++ ReadFileCSV/src/main/java/com/Main.java
@@ -8,8 +8,5 @@ public class Main {
     public static Read_CSV readFile = new Read_CSV();
     public static void main(String[] args) throws IOException {
         readFile.readFileCSV("companies.csv");
-        System.out.println("Hello");
-        System.out.println("Sài git lần 2");
-        System.out.println();
     }
 }
