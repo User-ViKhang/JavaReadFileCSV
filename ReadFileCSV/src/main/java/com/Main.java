81bd337c89cf9789266d4d8f5731d2469b9f4e25
User-ViKhang <vikhang11112002@gmail.com>
2023-10-03 19:51:51 +0700

    up

--- ReadFileCSV/src/main/java/com/Main.java
+++ ReadFileCSV/src/main/java/com/Main.java
@@ -8,12 +8,6 @@ public class Main {
     public static Read_CSV readFile = new Read_CSV();
     public static void main(String[] args) throws IOException {
         readFile.readFileCSV("companies.csv");
-        System.out.println("Hello");
-        System.out.println("Sài git lần 2");
-        System.out.println("Sua code demo");
-        int a = 8;
-        int b = 5;
-        System.out.println(a + b);
-        System.out.println("This is text in main branch");
+
     }
 }   
