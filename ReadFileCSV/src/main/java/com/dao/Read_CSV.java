c5501c953aba30e3fb14aca103f1b25643c9ca8a
User-ViKhang <vikhang11112002@gmail.com>
2023-10-03 23:42:00 +0700

    Function 3

--- ReadFileCSV/src/main/java/com/dao/Read_CSV.java
+++ ReadFileCSV/src/main/java/com/dao/Read_CSV.java
@@ -9,6 +9,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Read_CSV extends HandleReadFile<CSV_DTO>{
     @Override
@@ -49,6 +50,8 @@ public class Read_CSV extends HandleReadFile<CSV_DTO>{
         System.out.println("Function 2: ");
         long totalCapital = data.stream().filter(item -> item.getCountry().equals("CH") && item.isHeadQuarter()).mapToLong(item -> item.getCapital()).sum();
         System.out.println("Total value Capital with Country = CH: " + totalCapital);
+        System.out.println("Function 3: ");
+        data.stream().filter(item -> item.getCountry().equals("CH")).sorted((a, b) -> (int) (a.getCapital() - b.getCapital())).collect(Collectors.toList()).forEach(System.out::println);
 
     }
 }
